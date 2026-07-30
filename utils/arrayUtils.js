4e3d73927c3728d831559cb05f919c3a68d8cc97
Rohan <rohankirdak8756@gmail.com>
2026-07-30 09:10:16 +0000

    docs: update README with usage examples

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -641,3 +641,14 @@ function maxVal(arr) {
 function sum(arr) {
   return arr.reduce((a, b) => a + b, 0);
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
