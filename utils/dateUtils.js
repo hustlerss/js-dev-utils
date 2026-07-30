eb9fedd34125611d5a76156bc95df294e468c293
Rohan <rohankirdak8756@gmail.com>
2026-07-30 07:48:16 +0000

    docs: update README with usage examples

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -423,3 +423,12 @@ function getDayName(date = new Date()) {
 function isPast(date) {
   return new Date(date) < new Date();
 }
+
+/**
+ * Adds N days to a date
+ */
+function addDays(date, n) {
+  const d = new Date(date);
+  d.setDate(d.getDate() + n);
+  return d;
+}
