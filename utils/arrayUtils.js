c38611455b25bc0a24f4084f8977e5ec26130f37
Rohan <rohankirdak8756@gmail.com>
2026-08-08 05:03:58 +0000

    feat: add isEmpty array check

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -721,3 +721,14 @@ function isEmpty(arr) {
 function sum(arr) {
   return arr.reduce((a, b) => a + b, 0);
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
