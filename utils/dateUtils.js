f4a29d2117ed3e9ff68dbb913b33a3fd76666164
Rohan <rohankirdak8756@gmail.com>
2026-08-17 06:26:51 +0000

    feat: add isEmpty array check

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -529,3 +529,10 @@ function nowIST() {
 function getDayName(date = new Date()) {
   return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
 }
+
+/**
+ * Checks if a date is in the past
+ */
+function isPast(date) {
+  return new Date(date) < new Date();
+}
