37b99579451424fa75b17baba1c32443794466be
Rohan <rohankirdak8756@gmail.com>
2026-08-30 10:35:22 +0000

    docs: update README with usage examples

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -594,3 +594,10 @@ function nowIST() {
 function isPast(date) {
   return new Date(date) < new Date();
 }
+
+/**
+ * Returns the day name (Monday, Tuesday...)
+ */
+function getDayName(date = new Date()) {
+  return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
+}
