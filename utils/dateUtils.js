38682b7b5c913f670dfcdb94494ba111ae695cd1
Rohan <rohankirdak8756@gmail.com>
2026-09-04 09:14:14 +0000

    chore: update .gitignore

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -633,3 +633,10 @@ function addDays(date, n) {
 function getDayName(date = new Date()) {
   return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
 }
+
+/**
+ * Returns current IST timestamp as readable string
+ */
+function nowIST() {
+  return new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+}
