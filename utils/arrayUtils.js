d26fc0317570aec04e3379635a90b27d45505f6d
Rohan <rohankirdak8756@gmail.com>
2026-09-05 08:35:07 +0000

    fix: correct regex in validator

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -916,3 +916,13 @@ function shuffle(arr) {
 function sum(arr) {
   return arr.reduce((a, b) => a + b, 0);
 }
+
+/**
+ * Groups array items by a key
+ */
+function groupBy(arr, key) {
+  return arr.reduce((acc, item) => {
+    (acc[item[key]] = acc[item[key]] || []).push(item);
+    return acc;
+  }, {});
+}
