895b52104fd799fb836d14b63d87891c334ea1ca
Rohan <rohankirdak8756@gmail.com>
2026-09-06 09:09:08 +0000

    refactor: rename internal variables for clarity

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -933,3 +933,10 @@ function groupBy(arr, key) {
 function maxVal(arr) {
   return Math.max(...arr);
 }
+
+/**
+ * Sums all numbers in an array
+ */
+function sum(arr) {
+  return arr.reduce((a, b) => a + b, 0);
+}
